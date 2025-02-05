3ef19adbda6dddb9c1985d340ba94d2159a77f09
somerainbowinthedark <jmiri697@gmail.com>
2025-02-05 07:54:06 -0500

    i forgot that i was still on main

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,28 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
 
-        String [ ][ ] symbols = new String [11][11]
-        
-        for(int i = 0; i < symbols.length; i++) {
-            for(int j = 0; j < symbols[i].length ; j++){
-                symbols[i][j] = “O";
-                }
-            }
-        symbols[0][0] = “”;
-        symbols[10][10] = “”;
-        
-        
-            print2DArray(symbols);
-        }
-        
-        public static void print2DArray(String[ ][ ] array){
-        for(int i = 0; i < symbols.length; i++) {
-            for(int j = 0; j < symbols[i].length ; j++){
-                System.out.println(array[i][j]);
-                    }
-        System.out.println(x:””);
-            }
-        
         }
             
         
